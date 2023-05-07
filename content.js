51e92c3271bdd5589980d3d31a6a0a6966dbf777
Daniel Hückmann <sanitybit@gmail.com>
2023-05-07 00:33:15 -0700

    add domain validation / length checks

--- content.js
+++ content.js
@@ -5,12 +5,14 @@ const storage =
   typeof browser !== "undefined" ? browser.storage.local : chrome.storage.local
 
 // Regular expression to validate the DID format
+// Make sure that we don't DoS the regex if someone supplies too large of a DID.
+const MAX_DID_LENGTH = 1024
 const didRegex =
   /^did:plc:([a-zA-Z0-9._-]+(:[a-zA-Z0-9._-]+)*|((%[0-9A-Fa-f]{2})|[a-zA-Z0-9._-])+(:((%[0-9A-Fa-f]{2})|[a-zA-Z0-9._-])+)*$)/
 
 // Function to validate the DID string
 function isValidDID(didString) {
-  return didRegex.test(didString)
+  return didString.length <= MAX_DID_LENGTH && didRegex.test(didString)
 }
 
 // Function to get the domain name from the current hostname
@@ -19,26 +21,48 @@ function getDomainName() {
   return hostname.replace(/^www\./, "")
 }
 
+// Function to validate the domain name
+function isValidDomain(domain) {
+  const MAX_DOMAIN_LENGTH = 255
+
+  if (domain.length > MAX_DOMAIN_LENGTH) {
+    return false
+  }
+
+  try {
+    // Use the build in URL constructor to validate the URL, if doesn't throw an error, the domain is valid
+    // This is a better choice than a regex since it should properly support punycode/international domains
+    new URL(`https://${domain}`)
+    return true
+  } catch (error) {
+    // The URL constructor threw an error, so the domain is not valid
+    return false
+  }
+}
+
 // Function to check for a DID in the domain's TXT records
 async function checkForDIDDNS(domain) {
-  // We use Google's DNS over HTTPS API to resolve the TXT record
-  const response = await fetch(
-    `https://dns.google/resolve?name=_atproto.${domain}&type=TXT`
-  )
-  const data = await response.json()
+  try {
+    const response = await fetch(
+      `https://dns.google/resolve?name=_atproto.${domain}&type=TXT`
+    )
+    const data = await response.json()
 
-  // We use the TXT record type to avoid CORS issues
-  const records = data?.Answer?.filter((record) => record.type === 16) || []
+    // We use the TXT record type to avoid CORS issues
+    const records = data?.Answer?.filter((record) => record.type === 16) || []
 
-  // We filter out all records that are not TXT records
-  const didRecord = records.find((record) =>
-    record.data.includes("did=did:plc:")
-  )
+    // We filter out all records that are not TXT records
+    const didRecord = records.find((record) =>
+      record.data.includes("did=did:plc:")
+    )
 
-  // We return the DID if we found one and it's valid
-  return didRecord && isValidDID(didRecord.data.replace("did=", ""))
-    ? didRecord.data.replace("did=", "")
-    : null
+    // We return the DID if we found one and it's valid
+    return didRecord && isValidDID(didRecord.data.replace("did=", ""))
+      ? didRecord.data.replace("did=", "")
+      : null
+  } catch (error) {
+    return null
+  }
 }
 
 // Function to check for a DID in the well-known (not .well-known) location
@@ -47,6 +71,11 @@ async function checkForDIDHTTPS(domain) {
     const response = await fetch(
       `https://${domain}/xrpc/com.atproto.identity.resolveHandle`
     )
+
+    if (!response.headers.get("Content-Type")?.includes("application/json")) {
+      throw new Error("Invalid Content-Type")
+    }
+
     const data = await response.json()
     return data.did && isValidDID(data.did) ? data.did : null
   } catch (error) {
@@ -61,25 +90,30 @@ function performAction(privacyConsentAccepted) {
     // We check for a DID on the current domain
     ;(async function () {
       const domain = getDomainName()
-      const domainDID = await checkForDIDDNS(domain)
-      const httpsDID = await checkForDIDHTTPS(domain)
+      if (isValidDomain(domain)) {
+        const domainDID = await checkForDIDDNS(domain)
+        const httpsDID = await checkForDIDHTTPS(domain)
 
-      if (domainDID) {
-        runtime.sendMessage({ type: "DID_FOUND", did: domainDID })
-      } else if (httpsDID) {
-        runtime.sendMessage({ type: "DID_FOUND", did: httpsDID })
-      } else {
-        runtime.sendMessage({ type: "DID_NOT_FOUND" })
+        if (domainDID) {
+          runtime.sendMessage({ type: "DID_FOUND", did: domainDID })
+        } else if (httpsDID) {
+          runtime.sendMessage({ type: "DID_FOUND", did: httpsDID })
+        } else {
+          runtime.sendMessage({ type: "DID_NOT_FOUND" })
+        }
       }
     })()
 
     // We listen for messages from the background script
     runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (message.type === "GET_DID") {
-        checkForDIDDNS(getDomainName())
-          .then((did) => sendResponse({ did }))
-          .catch(() => sendResponse({ did: null }))
-        return true // Indicate that the response will be sent asynchronously.
+        const domain = getDomainName()
+        if (isValidDomain(domain)) {
+          checkForDIDDNS(domain)
+            .then((did) => sendResponse({ did }))
+            .catch(() => sendResponse({ did: null }))
+          return true // Indicate that the response will be sent asynchronously.
+        }
       }
     })
   } else {
